549419c6e521596342d77ac7e47a90090c2f5968
Ramanathan Narasimhan <rnarasimhan@amadeus.com>
2018-06-17 21:26:24 +0100

    Animal

---
+++ src/Package1/day1/Animal.java
@@ -0,0 +1,15 @@
+package Package1.day1;
+
+public class Animal {
+
+void bark() { 
+	 System.out.println ("woof-woof");
+}
+}
+
+class MyClass {
+  public static void main(String[ ] args) {
+      Animal dog = new Animal();
+      dog.bark();
+  }
+}
